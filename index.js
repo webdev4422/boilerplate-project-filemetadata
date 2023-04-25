1838efd4c2b5d653ff4eb0e8d6ff8b2fd534dddd
user <user@gmail.com>
2023-04-25 20:18:15 +0300

    Add multer, nodemon and response with req.file.*** json

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const app = express()
 
-var app = express();
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size })
+})
 
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
-});
+})
